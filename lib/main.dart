05c25fafbef881d30d6145a52d62d9aeafd032f5
strgzin <146336874+strgzin@users.noreply.github.com>
2023-11-27 12:24:54 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,6 @@ import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
 import 'package:myapp/page-1/icon-magnifying-glass.dart';
-// import 'package:myapp/page-1/home.dart';
-// import 'package:myapp/page-1/home-2.dart';
-// import 'package:myapp/page-1/jadwal-1.dart';
-// import 'package:myapp/page-1/jadwal.dart';
-// import 'package:myapp/page-1/form-konsutasi.dart';
-// import 'package:myapp/page-1/form-konsutasi-2.dart';
-// import 'package:myapp/page-1/pesan.dart';
-// import 'package:myapp/page-1/pesan-2.dart';
-// import 'package:myapp/page-1/profil.dart';
-// import 'package:myapp/page-1/profil-irm.dart';
-// import 'package:myapp/page-1/pengaduan.dart';
-// import 'package:myapp/page-1/notifikasi.dart';
-// import 'package:myapp/page-1/login.dart';
-// import 'package:myapp/page-1/sign-up.dart';
 
 void main() => runApp(MyApp());
 
